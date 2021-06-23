ec0fdaf355b7db52ab75a96ad1d787aebfff3b7a
Md. Towfikul Islam Talukder <towfikislam64@gmail.com>
2021-06-23 21:15:10 +0600

    initial commit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,17 +5,17 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
+  // {
+  //   path: '',
+  //   redirectTo: 'login',
+  //   pathMatch: 'full'
+  // },
   {
     path: '',
     component:  HomeComponent
   },
   {
-    path: 'login',
+    path: '',
     component: LoginComponent
   },
   {
